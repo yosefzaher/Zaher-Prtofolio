3808e232c797f4152141dd11b741aeb435dec3cc
yosefzaher <zaheryosef72@gmail.com>
2025-09-05 08:35:29 +0300

    Update The Name

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -7,12 +7,12 @@ export const HeroSection = () => {
       className="relative min-h-screen flex flex-col items-center justify-center px-4"
     >
       <div className="container max-w-5xl mx-auto z-10 flex flex-col md:flex-row items-center md:justify-between gap-8">
-        {/* النص */}
+        
         <div className="text-center md:text-left space-y-6">
           <h1 className="text-4xl md:text-6xl font-bold tracking-tight">
             <span className="opacity-0 animate-fade-in"> Hi, I'm</span>
-            <span className="text-primary opacity-0 animate-fade-in-delay-1"> Yosef</span>
-            <span className="text-gradient ml-2 opacity-0 animate-fade-in-delay-2"> Zaher</span>
+            <span className="text-primary opacity-0 animate-fade-in-delay-1"> Ahmed</span>
+            <span className="text-gradient ml-2 opacity-0 animate-fade-in-delay-2"> Mohamed</span>
           </h1>
 
           <p className="text-lg md:text-xl text-muted-foreground max-w-2xl opacity-0 animate-fade-in-delay-3">
